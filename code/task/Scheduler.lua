e4a64dbb382d75731efde73f2d97128c57225334
BRY402 <105960244+BRY402@users.noreply.github.com>
2025-12-29 23:59:42 -0300

    Update Scheduler.lua
    Changed args in addTask to be table only

--- code/task/Scheduler.lua
+++ code/task/Scheduler.lua
@@ -40,25 +40,29 @@ function scheduler.addFloor(level)
     return scheduler.tasks[level]
 end
 
-function scheduler.addTask(thread, level, condition, ...)
+function scheduler.addTask(thread, level, condition, args)
     local tasks = scheduler.tasks
     local floor, err = tasks[level] or scheduler.addFloor(level)
     if not floor then
         return nil, err
     end
     
-    if type(thread) ~= 'thread' then
+    if type(thread) ~= "thread" then
         return nil, "Argument #1 ÷xpected thread, got "..type(thread)
     end
     
     if type(condition) ~= "function" then
         return nil, "Argument #3 expected function, got "..type(condition)
     end
+    
+    if args and type(args) ~= "table" then
+        return nil, "Argument #4 expected table, got "..type(args)
+    end
 
     floor[floor.n + 1] = {
         thread = thread,
         createdAt = os_clock(),
-        args = {...},
+        args = args or {},
         finished = false,
         condition = condition,
         index = floor.n + 1
@@ -77,12 +81,12 @@ function scheduler.removeTask(floor, i)
     floor.n = floor.n - 1
 end
 
-function scheduler.scheduleTask(thread, level, waitTime, ...)
+function scheduler.scheduleTask(thread, level, waitTime, args)
     return scheduler.addTask(thread, level, function(self)
         self.finished = os_clock() - self.createdAt >= waitTime
         
         return self.finished
-    end)
+    end, args)
 end
 
 local function execTask(scheduledTask, floor)
@@ -100,63 +104,6 @@ local function execTask(scheduledTask, floor)
     
     if scheduledTask.finished then
         scheduler.removeTask(floor, scheduledTask.index)
-    end
-    
-    return true
-end
-
-
-function scheduler.run(level, i)
-    if not isInt(level) then
-        return false, "Level is expected to be an integer"
-    end
-
-    local tasks = scheduler.tasks
-    local floor = tasks[level]
-
-    if not floor then
-        return false, "Given level for floor is invalid (no floor found)"
-    end
-
-    if i then
-        if not isInt(i) then
-            return false, "Index expected to be an integer"
-        end
-
-        local task = floor[i]
-
-        return execTask(task, floor)
-    end
-
-    for _, task in ipairs(floor) do
-        if task then
-            execTask(task, floor)
-        end
-    end
-
-    return true
-end
-
-scheduler.resume = resume
-
-return scheduler    end)
-end
-
-local function execTask(scheduledTask, floor)
-    if not scheduledTask then
-        return false, "Scheduled task is nil"
-    end
-    
-    if not scheduledTask.finished and scheduledTask:condition() then
-        local thread = scheduledTask.thread
-        
-        if coroutine_status(thread) ~= 'dead' then
-            resume(scheduledTask.thread, table_unpack(scheduledTask.args))
-        end
-    end
-    
-    if scheduledTask.finished then
-        scheduler.removeTask(floor, scheduledTask.Index)
     end
     
     return true
